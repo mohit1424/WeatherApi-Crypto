31a8dedc501ca7d30cf316646c920f680f9d9653
Mohit Baghel <mohitbaghel1413@gmail.com>
2025-04-02 06:32:41 +0530

    adding Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Weather from './Component/Weather'
-
+import Home from './Component/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <Weather/>
+   <Home/>
     </>
   )
 }
